8ab4f017eda4e81faa2efd0d95151352ac6a432d
Abhishek Kumar <kumar.abhishek@techie.com>
2015-07-25 00:24:47 +0530

    fixed path home action

--- controllers/page_controller.php
+++ controllers/page_controller.php
@@ -3,7 +3,7 @@ class PageController {
 	public function home() {
 		$first_message = "Hi";
 		$second_message  = "Welcome";
-		require_once('dispay/page/home.php');
+		require_once('display/page/home.php');
 	}
 	public function error(){
 		require_once('display/page/error.php');	
