c6d1e14d8243f80aa53baa887168e2d39d493711
Abhishek Kumar <kumar.abhishek@techie.com>
2015-07-25 01:32:11 +0530

    add post_controller

---
+++ controllers/post_controller.php
@@ -0,0 +1,20 @@
+<?php
+	class PostConroller {
+		public function index() {
+			$posts = Post::all()
+			require_once()"display/post/index.php");
+		}
+
+		public function show() {
+			// expecing a url form ?controller=posts&action=show&id=x
+			// without id redirect to error page
+			if(!isset($_GET['id'])){
+				return call('page', 'error');
+			}
+
+			// using given id to find the correct post
+			$post = Post::find($_GET['id']);
+			require_once('display/post/show.php');
+		}
+	}
+?>
